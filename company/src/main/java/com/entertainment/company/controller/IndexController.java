41841e84d9552086689ef5ffc69fb418659306bf
zhi.zhang <zhi.zhang@chinaredstar.com>
2017-12-28 17:58:41 +0800

    配置修改

--- company/src/main/java/com/entertainment/company/controller/IndexController.java
+++ company/src/main/java/com/entertainment/company/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.entertainment.company.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by 张智
@@ -10,4 +12,11 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class IndexController {
+
+    @GetMapping("/index")
+    public ModelAndView index(ModelAndView mv){
+        mv.addObject("username","ABC");
+        mv.setViewName("login");
+        return mv;
+    }
 }
---
+++ company/src/main/resources/templates/login.ftl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Demo1</title>
+</head>
+<body>
+    <h1>${username}</h1>
+</body>
+<script>
+</script>
+</html>
