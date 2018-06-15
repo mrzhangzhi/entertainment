481f07c5e07d3cfac2ef7aa7621cc23ad83569a4
zhi.zhang <zhi.zhang@chinaredstar.com>
2018-06-15 16:08:13 +0800

    搭建dubbo

---
+++ api/src/main/java/DemoServiceApi.java
@@ -0,0 +1,6 @@
+/**
+ * Created by zhi.zhang Date: 2018/6/15 Time: 10:29 Description:
+ */
+public interface DemoServiceApi {
+    String say(String name);
+}
---
+++ sso/src/main/java/com/example/sso/dubbo/service/DemoService.java
@@ -0,0 +1,8 @@
+package com.example.sso.dubbo.service;
+
+
+/**
+ * Created by zhi.zhang Date: 2018/6/15 Time: 10:32 Description:
+ */
+public class DemoService {
+}
